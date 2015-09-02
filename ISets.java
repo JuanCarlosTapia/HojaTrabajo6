417d9534fded90ff79f956b67a8ee24f3c33106c
ldv10 <ninleo10@hotmail.com>
2015-09-01 19:45:13 -0600

    Update ISets.java

--- ISets.java
+++ ISets.java
@@ -1,3 +1,8 @@
+// Universidad del Valle de Guatemala
+// Algoritmos y estructuras de datos
+// Hoja de trabajo 6 
+// Juan Carlos Tapia Flores 14133
+// Leonel Guillen del Valle 14451
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -30,9 +35,17 @@ public class ISets {
   System.out.println("2. TreeSet");
   System.out.println("3. LinkedHashSet");
   int opcion = teclado.nextInt();//opcion lleva el numero del set que se eligio
+  while  (opcion != 3 && opcion != 2 && opcion!= 1){
+	System.out.println("Por favor ingrese una opcion valida:");
+	System.out.println("Intente de nuevo");	
+	opcion = teclado.nextInt();
+  } 
+ 
+ 
   Set setElegido = setFactory.getSet(opcion);
   teclado.nextLine();
-           
+ 
+	
             
 																		
   int seguir = 1;
@@ -43,15 +56,26 @@ public class ISets {
   			//se pregunta por conocimientos de Java
   			System.out.println("Este desarrollador tiene experiencia enJava?  1.Si    2.No");
   			int SabeJava = teclado.nextInt();
+			 while  (SabeJava != 1 && SabeJava != 2){
+			System.out.println("Por favor ingrese una opcion valida:");
+			 System.out.println("Intente de nuevo");
+			SabeJava = teclado.nextInt();
+			 }
+			 
   			if (SabeJava==1){
   				java = true;
   			}
-  			else{
+  			else {
   				java = false;
   			}
   		//se pregunta por conocimientos de Web
   			System.out.println("Este desarrollador tiene experiencia en Web?  1. Si   2. No");
   			int SabeWeb = teclado.nextInt();
+			 while  (SabeWeb != 1 && SabeWeb != 2){
+			System.out.println("Por favor ingrese una opcion valida:");
+			 System.out.println("Intente de nuevo");
+			SabeWeb = teclado.nextInt();
+			 }
   			if (SabeWeb==1){
   				web = true;
   			}
@@ -62,6 +86,11 @@ public class ISets {
   		//se pregunta por conocimientos de Celulares
   			System.out.println("Este desarrollador tiene experiencia en Celulares?     1.Si    2.No");
   			int SabeCel = teclado.nextInt();
+			 while  (SabeCel != 1 && SabeCel != 2){
+			System.out.println("Por favor ingrese una opcion valida:");
+			 System.out.println("Intente de nuevo");
+			SabeCel = teclado.nextInt();
+			 }
   			if (SabeCel==1){
   				celular =true;
   			}
@@ -84,6 +113,11 @@ public class ISets {
   		//se pregunta si se quiere agregar mas programadores
   			System.out.println("Quiere agregar? 1.Si  2.No");
   			seguir = teclado.nextInt();
+			 while  (seguir != 1 && seguir != 2){
+			System.out.println("Por favor ingrese una opcion valida:");
+			 System.out.println("Intente de nuevo");
+			seguir = teclado.nextInt();
+			 }
   			teclado.nextLine();
                    
   		}
